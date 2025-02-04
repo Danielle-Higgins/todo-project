d1630db15798b48f31adf94f741b6d74b56ceb19
Danielle Higgins <higdan4024@gmail.com>
2025-02-04 17:39:24 -0500

    html and css revamp

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
-const userInput = document.querySelector("input")
-const addButton = document.querySelector(".add-button")
-const todos = document.querySelector(".task-container")
+// const userInput = document.querySelector("input")
+// const addButton = document.querySelector(".add-button")
+// const todos = document.querySelector(".task-container")
 
-function addTask() {
-    // create a new task
-    const task = document.createElement("div")
-    task.classList.add("task")
+// function addTask() {
+//     // create a new task
+//     const task = document.createElement("div")
+//     task.classList.add("task")
 
-    // if user doesnt input anything, break out of function
-    if (userInput.value === "") {
-        alert("Please enter a task first!")
-        return
-    }
+//     // if user doesnt input anything, break out of function
+//     if (userInput.value === "") {
+//         alert("Please enter a task first!")
+//         return
+//     }
 
-    // create paragraph to house value of input
-    const para = document.createElement("p")
-    para.textContent = userInput.value
-    task.appendChild(para)
+//     // create paragraph to house value of input
+//     const para = document.createElement("p")
+//     para.textContent = userInput.value
+//     task.appendChild(para)
 
-    // create a check mark button
-    const check = document.createElement("button")
-    check.classList.add("check-mark")
-    check.innerHTML = " &#10003;"
-    task.appendChild(check)
+//     // create a check mark button
+//     const check = document.createElement("button")
+//     check.classList.add("check-mark")
+//     check.innerHTML = " &#10003;"
+//     task.appendChild(check)
 
-    // create a trash can button
-    const trash = document.createElement("button")
-    trash.classList.add("trash-can")
-    trash.innerHTML = "&#128465;"
-    task.appendChild(trash)
+//     // create a trash can button
+//     const trash = document.createElement("button")
+//     trash.classList.add("trash-can")
+//     trash.innerHTML = "&#128465;"
+//     task.appendChild(trash)
 
-    todos.appendChild(task)
+//     todos.appendChild(task)
 
-    // reset the input
-    userInput.value = ""
+//     // reset the input
+//     userInput.value = ""
 
-    check.addEventListener("click", () => {
-        para.style.textDecoration = "line-through"
-    })
+//     check.addEventListener("click", () => {
+//         para.style.textDecoration = "line-through"
+//     })
 
-    trash.addEventListener("click", () => {
-        task.remove()
-    })
-}
+//     trash.addEventListener("click", () => {
+//         task.remove()
+//     })
+// }
 
-addButton.addEventListener("click", addTask)
+// addButton.addEventListener("click", addTask)
